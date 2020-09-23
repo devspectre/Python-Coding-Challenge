4c75a3bf243a2638cd3d77e048fc7494d6fd0e42
devspectre <devspectre@yandex.com>
2020-09-24 00:25:08 +0800

    added solution for question 2

---
+++ 2.py
@@ -0,0 +1,22 @@
+import sys
+import re
+
+sample_input = "11:22:33:44:55:66 aaa\na6:ae:2f:9d:31:b4\nbbb\n"
+
+pattern = '([a-f0-9]{2}[:]{1}){5}([a-f0-9]{2}){1}'
+
+def validate_lines(string: str) -> list:
+	result = []
+	lines = string.split('\n')
+	for line in lines:
+		match = re.compile(pattern).finditer(line)
+		if match:
+			for y in match:
+				found = line[y.start():y.end()]
+				if found == line.strip():
+					result.append(line)
+
+
+	return result
+
+sys.stdout.write(validate_lines(sample_input))
